378271d4f66f701f54103ed8fe543ca17a877180
Shaurya Benipuri <shauryabenipuri@Shauryas-MacBook-Air-3.local>
2026-09-17 01:25:43 -0400

    test file

---
+++ utils.py
@@ -0,0 +1,20 @@
+class utils:
+    @staticmethod
+    def reversed(number):
+        if type(number) is not int:
+            raise TypeError("number must be an integer")
+
+        sign = -1 if number < 0 else 1
+        reversed_number = int(str(abs(number))[::-1])
+
+        return sign * reversed_number
+
+    @staticmethod
+    def formatter(number):
+        if type(number) is not int:
+            raise TypeError("number must be an integer")
+
+        binary = bin(number)
+        octal = oct(number)
+
+        return binary, octal
